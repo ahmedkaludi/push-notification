9f49d32769c0bc1a565112290c5f695cf612d8ec
Ajeet <ajeet.gupta51@gmail.com>
2020-01-28 19:33:06 +0530

    Fix if API key is not entered. so do not write the code in service worker
    Fix if API key is not entered. so do not write the code in service worker

--- inc/admin/admin.php
+++ inc/admin/admin.php
@@ -29,7 +29,10 @@ class Push_Notification_Admin{
 		if( function_exists('pwaforwp_init_plugin') ){
 			/** service worker change */
 			$settings = pwaforwp_defaultSettings();
-			if($settings['notification_feature']==1 && $settings['notification_options']=='pushnotifications_io'){
+			$auth_settings = push_notification_auth_settings();
+			if($settings['notification_feature']==1 && $settings['notification_options']=='pushnotifications_io' 
+				//For Auth settings check
+				&& isset($auth_settings['user_token']) && isset($auth_settings['token_details']['validated']) && $auth_settings['token_details']['validated'] == 1){
 				add_filter( "pwaforwp_sw_js_template", array($this, 'add_sw_js_content') , 10, 1);
 				add_filter( "pwaforwp_pn_config", array($this, 'add_pn_config') , 10, 1);
 				add_filter( "pwaforwp_pn_use_sw", array($this, 'add_pn_use_sw') , 10, 1);
@@ -48,10 +51,11 @@ class Push_Notification_Admin{
 	* 
 	*/
 	function add_sw_js_content($swJsContent){
-		$messageSw = $this->pn_get_layout_files('messaging-sw.js');
+        	$messageSw = $this->pn_get_layout_files('messaging-sw.js');
 			$settings = $this->json_settings();
 			$messageSw = str_replace('{{pnScriptSetting}}', json_encode($settings), $messageSw);
-		$swJsContent .= $messageSw;
+			$swJsContent .= $messageSw;
+		
 		return $swJsContent;
 	}
 
