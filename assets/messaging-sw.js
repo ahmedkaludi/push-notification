2310e5137d55110a2a86eac4c405f1bd7cf40424
Ajeet kumar <ajeet.gupta51@gmail.com>
2021-09-20 13:49:12 +0530

    added badge for notification

--- assets/messaging-sw.js
+++ assets/messaging-sw.js
@@ -5,6 +5,7 @@ var pnScriptSetting = {{pnScriptSetting}}
 var config=pnScriptSetting.pn_config;   
 if (!firebase.apps.length) {firebase.initializeApp(config);}		  		  		  
 const messaging = firebase.messaging();
+var messageCount = 0;
 
 messaging.setBackgroundMessageHandler(function(payload) {  
 const notificationTitle = payload.data.title;
@@ -19,6 +20,8 @@ const notificationOptions = {
 					url : payload.data.url
 				  },
 				}
+				messageCount += 1;
+				setBadge(messageCount);
 	return self.registration.showNotification(notificationTitle, notificationOptions); 
 
 });
@@ -26,6 +29,12 @@ const notificationOptions = {
 self.addEventListener("notificationclose", function(e) {
 var notification = e.notification;
 var primarykey = notification.data.primarykey;
+	messageCount -= 1;
+	if(messageCount>0){
+		setBadge(messageCount);
+	}else{
+		clearBadge();
+	}
 console.log("Closed notification: " + primarykey);
 });
 
@@ -39,4 +48,32 @@ if (action === "close") {
   clients.openWindow(notification.data.url);
   notification.close();
 }
+	messageCount -= 1;
+	if(messageCount>0){
+		setBadge(messageCount);
+	}else{
+		clearBadge();
+	}
 });  
+
+function setBadge(...args) {
+  if (navigator.setAppBadge) {
+    navigator.setAppBadge(...args);
+  } else if (navigator.setExperimentalAppBadge) {
+    navigator.setExperimentalAppBadge(...args);
+  } else if (window.ExperimentalBadge) {
+    window.ExperimentalBadge.set(...args);
+  }
+}
+
+// Wrapper to support first and second origin trial
+// See https://web.dev/badging-api/ for details.
+function clearBadge() {
+  if (navigator.clearAppBadge) {
+    navigator.clearAppBadge();
+  } else if (navigator.clearExperimentalAppBadge) {
+    navigator.clearExperimentalAppBadge();
+  } else if (window.ExperimentalBadge) {
+    window.ExperimentalBadge.clear();
+  }
+}
