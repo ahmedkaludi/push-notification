0fce943fde04d33371823db70dcfaafc1e1feb78
Akshay Wali <akshaywali@live.in>
2024-03-16 13:30:37 +0530

    remove All Categories and specific category  dependency

--- assets/main-admin-script.js
+++ assets/main-admin-script.js
@@ -264,9 +264,7 @@ jQuery(document).ready(function($){
 		if(jQuery(this).prop("checked")==true){
 			jQuery("#category_selector_wrapper").show();
 			jQuery(".js_category_selector_wrapper").show();
-			if(jQuery('#pn_push_segment_on_category_checkbox').prop("checked")==true){
-				jQuery(".js_custom_category_selector_wrapper").show();
-			}
+			jQuery(".js_custom_category_selector_wrapper").show();
 		}else{
 			jQuery("#category_selector_wrapper").hide();
 			jQuery(".js_category_selector_wrapper").hide();
@@ -274,20 +272,6 @@ jQuery(document).ready(function($){
 		}
 	});
 
-	jQuery('#pn_push_segment_on_category_checkbox').click(function(){
-		if(jQuery(this).prop("checked")==true){
-			jQuery("#js_category").prop('disabled',false);
-			jQuery("#js_category_hidden").prop('disabled',true);
-			jQuery("#segment_category_selector_wrapper").show();
-			jQuery(".js_custom_category_selector_wrapper").show();
-		}else{
-			jQuery(".js_custom_category_selector_wrapper").hide();
-			jQuery("#segment_category_selector_wrapper").hide();
-			jQuery("#js_category").prop('disabled',true);
-			jQuery("#js_category_hidden").prop('disabled',false);
-		}
-
-	});
 
 	jQuery(".push-notification-tabs a").click(function(e){
 	        e.preventDefault();
